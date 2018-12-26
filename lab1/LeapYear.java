908af5a4a4345b177096f3628241558b455a8c03
gabriel <gabriel4k2@gmail.com>
2018-12-26 14:27:14 -0200

    LeapYear completado

---
+++ lab1/LeapYear.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int year = 2100;
+
+        if(year % 400 == 0 || ( (year %4 == 0 ) && ( (year % 100) != 0)) ){
+
+
+                System.out.println( year + " is a leap year!" );
+        }
+
+        else{
+            System.out.println( year + "is  NOT a leap year!" );
+
+
+        }
+
+
+
+    }
+}
